10b6773bffc091919af99e1647bfcf88762067ac
Themperror <Colacolaboy@hotmail.com>
2018-03-08 16:07:59 +0100

    Delete ScreenSpace_ps.hlsl

--- shaders/ScreenSpace_ps.hlsl
+++
@@ -1,8 +0,0 @@
-#include "Defines.hlsl"
-
-float4 PShader(VS_OUTPUT input) : SV_TARGET
-{
-    float4 textureColor = float4(0.0, 1.0, 0.0, 0.0); // = shaderTexture[0].Sample(SampleType[0], uv);
-
-    return textureColor;
-}
